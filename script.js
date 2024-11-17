bd3c1ae5e3b4854e572be13f5a8204a15ea3d8c5
wajahathusssain <whussain.171727@gmail.com>
2024-11-17 19:48:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function sendFriendRequest(username) {
+    alert(`Friend request sent to ${username}!`);
+}
+
+function createServer() {
+    alert("Server creation functionality coming soon!");
+}
+
+function createChannel() {
+    alert("Channel creation functionality coming soon!");
+}
+
+function startVoiceCall() {
+    alert("Starting voice call...");
+}
+
+function startVideoCall() {
+    alert("Starting video call...");
+}
+
+function react(element, emoji) {
+    alert(`Reacted with ${emoji}`);
+}
